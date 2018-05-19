7ac54e224afb69abc1e467769d717af54bd644b4
算神 <lishewen@msn.com>
2018-05-19 10:16:28 +0800

    搜索功能

--- src/app/dispatch/dispatch.component.ts
+++ src/app/dispatch/dispatch.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DispatchService } from './dispatch.service';
+import { isNull } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-dispatch',
@@ -8,11 +9,23 @@ import { DispatchService } from './dispatch.service';
 })
 export class DispatchComponent implements OnInit {
   list: models.调度汇总[] = [];
+  private data: models.调度汇总[] = [];
 
   constructor(private dispatchService: DispatchService) { }
 
   ngOnInit() {
     this.dispatchService.getDispatch()
-      .subscribe((data: Array<models.调度汇总>) => this.list = data);
+      .subscribe((data: Array<models.调度汇总>) => {
+        this.data = data;
+        this.onSearch('');
+      });
+
+  }
+  onSearch(str: string) {
+    this.list = this.data.filter((w: models.调度汇总) =>
+      ~w.onBoardid.toString().indexOf(str)
+      || ~w.name.indexOf(str)
+      || ~w.lineName.indexOf(str)
+      || (w.stationName != null && ~w.stationName.indexOf(str)));
   }
 }
