7844b8acf88f3280cbcc463380b7335fca7dd2d0
算神 <lishewen@msn.com>
2019-07-01 14:32:32 +0800

    ng update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { WeUiModule, ToptipsService } from 'ngx-weui';
+import { WeUiModule } from 'ngx-weui';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
